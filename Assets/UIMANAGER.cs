5615c4e5a640d2465e77d2f58b6910f382159ca3
3Nina <167197662+3Nina@users.noreply.github.com>
2025-05-05 09:04:37 -0300

    TxtSaludo Vacio

---
+++ Assets/UIMANAGER.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIMANAGER : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo; // Es el componente que se quiere modificar
+
+    void Start()
+    {
+        txtSaludo.text = ""; //.text porque es la propiedad
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
